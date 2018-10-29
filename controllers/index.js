2022f38cb7cfa697833b3629cd4db063475e6b21
wyq <151260636@qq.com>
2018-10-29 20:48:43 +0800

    [update][code]: add checking database for logging in.

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,8 @@
 
+const config = require('../config')
+const Sequelize = require('sequelize')
+const crypto = require('crypto')
+
 
 var fn_index = async(ctx, next) => {
     // ctx.response.body = `<h1>Index</h1>
@@ -11,25 +15,75 @@ var fn_index = async(ctx, next) => {
 }
 
 
+var conn = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'mysql',
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 30000
+    }
+});
+
+
+var admins = conn.define('admins', {
+    name: {
+        type: Sequelize.STRING(20),
+        primaryKey: true,
+    },
+    passwd: Sequelize.STRING(32),
+    create_time: Sequelize.TIME,
+    comment: Sequelize.STRING(30)
+    },
+    {
+        timestamps: false
+    })
+
+
 var fn_signin = async(ctx, next) => {
     var name = ctx.request.body.name || ''
     var password = ctx.request.body.password || ''
+    var passwdMd5 = crypto.createHash('md5').update(password).digest('hex')
 
     console.log(`Singin with name: ${name}, password: ${password}`)
 
-    if(name === 'koa@163.com' && password === '12345'){
-        // ctx.response.body = `<h1>Welcome, ${name}`
-        ctx.render('signin-ok.html', {
-            title: 'Sign In OK',
-            name: 'Mr Node'
-        });
-    }else{
-        // ctx.response.body = `<h1>Login failed!</h1>
-        // <p><a href='/'>try again</a></p>`
-        ctx.render('signin-failed.html', {
-            title: 'Sign In Failed'
-        });
-    }
+    //Search the db
+    await (async () => {
+        var rows = await admins.findAll({
+            where: {
+                name: name,
+            }
+        })
+        console.log(`>>Found ${rows.length} admins`)
+
+        var valid = false;
+        if(rows && rows.length > 0){
+            for(let a of rows){
+                console.log(JSON.stringify(a))
+                console.log('password=' + passwdMd5)
+                if(a.passwd == passwdMd5){
+                    valid = true
+                    break
+                }
+            }
+        }
+
+        
+        // if(name === 'koa@163.com' && password === '12345'){
+        if(valid){
+            // ctx.response.body = `<h1>Welcome, ${name}`
+            ctx.render('signin-ok.html', {
+                title: 'Sign In OK',
+                name: 'Mr Node'
+            });
+        }else{
+            // ctx.response.body = `<h1>Login failed!</h1>
+            // <p><a href='/'>try again</a></p>`
+            ctx.render('signin-failed.html', {
+                title: 'Sign In Failed'
+            });
+        }
+    })();
 }
 
 
